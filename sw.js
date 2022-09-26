ab541193d0bb12cdf78e718b5095d6d7368e01f6
Sumit Bhardwaj <sumitbhardwaj@Sumits-MacBook-Air.local>
2022-09-26 06:28:36 +0530

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-0cd2b8c1c2a66845f04b.js"
+    "url": "webpack-runtime-f7567653bab2c9126a6e.js"
   },
   {
     "url": "framework-deb5402af5b406c6372a.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "11e1ef8452262c86749414afb3aaadc2"
+    "revision": "96efc5010c562c017ab64d296a10611b"
   },
   {
     "url": "polyfill-143d49d662020a2c412a.js"
